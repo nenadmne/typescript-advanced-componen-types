87f8a921dbbabb0e9be33c4b12930e8292163605
nenadmne <cosovicnenad14@gmail.com>
2023-11-11 15:39:44 +0100

    Added button/anchor compontent

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Input from "./components/Input";
+import Button from "./components/button";
 
 function App() {
   return (
@@ -6,6 +7,12 @@ function App() {
       <h1>Let's get started!</h1>
       <Input id="1" label="Input field" type="text" disabled={true} />
       <Input id="2" label="Another input field" type="text" disabled={true} />
+      <p>
+        <Button el="anchor"> Link </Button>
+      </p>
+      <p>
+        <Button el="button"> Button </Button>
+      </p>
     </>
   );
 }
